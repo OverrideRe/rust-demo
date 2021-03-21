36255f703a257014d958318059bdce558cbe680f
yinghao <yinghao.re@gmail.com>
2021-03-21 22:29:47 +0800

    update

--- hello_cargo/src/struct_demo.rs
+++ hello_cargo/src/struct_demo.rs
@@ -1,11 +1,17 @@
 fn main() {
-    let user = User {
+    let mut user = build_user();
+    println!("user name is : {}", user.user_name);
+    user.user_name = String::from("Jiang Yinghao");
+    println!("user name is : {}", user.user_name);
+}
+
+fn build_user() -> User {
+    User {
         user_name: String::from("yinghao"),
         age: 26,
         email: String::from("yinghao.re@gmail.com"),
         active: true,
-    };
-    println!("user name is : {}", user.user_name);
+    }
 }
 
 struct User {
