0021b2d3547a0dce111fec2fb463318ab96145d7
yinghao <yinghao.re@gmail.com>
2021-04-18 18:25:23 +0800

    update

---
+++ rust_test/target/debug/deps/rust_test-c3b5cf3c8ac8642a.d
@@ -0,0 +1,5 @@
+/mnt/e/Doc/vscode-projects/rust-demo/rust_test/target/debug/deps/rust_test-c3b5cf3c8ac8642a: src/main.rs
+
+/mnt/e/Doc/vscode-projects/rust-demo/rust_test/target/debug/deps/rust_test-c3b5cf3c8ac8642a.d: src/main.rs
+
+src/main.rs:
