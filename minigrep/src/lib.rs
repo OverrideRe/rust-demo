fa1924b452562713820364cfc9755b0becb60f86
yinghao <yinghao.re@gmail.com>
2021-04-27 23:30:19 +0800

    完成minigrep编写

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,10 +1,12 @@
 
 use std::fs;
 use std::error::Error;
+use std::env;
 
 pub struct Config {
     pub query: String,
-    pub file_name: String
+    pub file_name: String,
+    pub case_sensitive: bool
 }
 
 impl Config {
@@ -12,12 +14,43 @@ impl Config {
         if args.len() < 3 {
             return Err("not enough arguments");
         }
-        Ok(Config { query: args[1].clone(), file_name: args[2].clone() })
+        Ok(Config { 
+            query: args[1].clone(), 
+            file_name: args[2].clone(), 
+            case_sensitive: env::var("CASE_SENSITIVE").is_err() 
+        })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_name)?;
-    println!("With text: \n{}", contents);
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_insensitive(&config.query, &contents)
+    };
+    for line in results {
+        println!("{}", line);
+    }
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = vec![];
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+    result
+}
+
+pub fn search_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = vec![];
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query.to_lowercase()) {
+            result.push(line);
+        }
+    }
+    result
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,12 +6,39 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     
+    // eprintln!会将标准错误输出到屏幕，标准输出则写入后面跟着的文件
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing argument: {}", err);
+        eprintln!("Problem parsing argument: {}", err);
         process::exit(1);
     });
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let content = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], minigrep::search(query, content))
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let content = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(vec!["Rust:", "Trust me."], minigrep::search_insensitive(query, content))
     }
 }
