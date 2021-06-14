6ba2465d2d3e1bf08ed8943f0a4ffdd7b9e3c1dd
Yinghao <yinghao.re@gmail.com>
2021-06-14 23:19:04 +0800

    自建迭代器

--- hello_cargo/src/iter/iter2_filter.rs
+++ hello_cargo/src/iter/iter2_filter.rs
@@ -21,3 +21,51 @@ fn filters_by_size() {
     let v2 = shoes_in_my_size(v1, 41);
     assert_eq!(v2, vec![Shoe {size: 41, style: String::from("sandal")}, Shoe {size: 41, style: String::from("boot")}])
 }
+
+// 自定义迭代器
+
+#[derive(Debug)]
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+        if self.count < 6 {
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+#[test]
+fn counter_next() {
+    let mut counter = Counter::new();
+
+    assert_eq!(counter.next(), Some(1));
+    assert_eq!(counter.next(), Some(2));
+    assert_eq!(counter.next(), Some(3));
+    assert_eq!(counter.next(), Some(4));
+    assert_eq!(counter.next(), Some(5));
+    assert_eq!(counter.next(), None);
+
+    // 1  2 3 4 5   
+    // 2 3 4 5      1 * 2 = 2, 2 * 3 = 6, 3 * 4 = 12, 4 * 5 = 20，其中只有6和12能被3整除，所以只剩这两个相加，得18
+    // 最后一对 5和None是没有的，因为zip对任一迭代器返回None就全都返回None
+    let total: u32 = Counter::new().zip(Counter::new().skip(1)).
+                                    map(|(a, b)| a * b)
+                                    .filter(|x| x % 3 == 0)
+                                    .sum();
+    assert_eq!(total, 18);
+}
